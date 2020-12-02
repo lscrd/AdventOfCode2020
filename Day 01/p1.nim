d5d7174c20be683375cce3aa5896e5af97efbef2
Luc Secouard <luc.secouard@laposte.net>
2020-12-02 10:15:25 +0100

    Add files via upload

---
+++ Day 01/p1.nim
@@ -0,0 +1,24 @@
+import strutils
+
+var data: seq[int]
+for line in "data".lines():
+  data.add(parseInt(line))
+
+func part1(data: seq[int]): int =
+  for i in 0..<data.high:
+    let val = data[i]
+    for j in i..data.high:
+      if val + data[j] == 2020:
+        return val * data[j]
+
+func part2(data: seq[int]): int =
+  for i in 0..(data.high-2):
+    let val1 = data[i]
+    for j in 1..<data.high:
+      let val2 = data[j]
+      for k in 2..data.high:
+        if val1 + val2 + data[k] == 2020:
+          return val1 * val2 * data[k]
+
+echo "Part 1: ", data.part1()
+echo "Part 2: ", data.part2()
