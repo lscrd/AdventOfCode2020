be53cba27b66f80ab96e6c7f6b9236c685ecd339
Luc Secouard <luc.secouard@laposte.net>
2020-12-04 09:31:28 +0100

    Add files via upload

---
+++ Day 03/p3.nim
@@ -0,0 +1,33 @@
+
+type Slope = tuple[right, down: int]
+
+var areaMap: seq[string]
+
+for line in "data".lines:
+  areaMap.add(line)
+
+
+func treeCount(areaMap: seq[string]; slope: Slope): int =
+  ## Return the number of trees encountered in the area when using the given slope.
+
+  let rowLen = areaMap.len
+  let colLen = areaMap[0].len
+
+  var row, col = 0
+  while true:
+    col = (col + slope.right) mod colLen
+    inc row, slope.down
+    if row >= rowLen:
+      break
+    if areaMap[row][col] == '#':
+      inc result
+
+#———————————————————————————————————————————————————————————————————————————————————————————————————
+
+var result =  areaMap.treeCount((3, 1))
+echo "Part 1: ", result
+
+
+for slope in [(1, 1), (5, 1), (7, 1), (1, 2)]:
+  result *= areaMap.treeCount(slope)
+echo "Part 2: ", result
