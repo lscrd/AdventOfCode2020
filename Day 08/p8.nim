f6afbcac192311ecbf320cf48f12282c6721c3e2
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:07:44 +0200

    Update p8.nim

--- Day 08/p8.nim
+++ Day 08/p8.nim
@@ -1,13 +1,8 @@
-import strutils
+import std/strutils
 
 type
-
-  OpCode = enum
-    opNop = "nop"
-    opAcc = "acc"
-    opJmp = "jmp"
-
-  Instruction = tuple[op: OpCode, offset: int]
+  OpCode = enum opNop = "nop", opAcc = "acc", opJmp = "jmp"
+  Instruction = tuple[op: OpCode, val: int]
 
 
 iterator steps(code: seq[Instruction]): tuple[pc: int16; acc: int] =
@@ -22,20 +17,33 @@ iterator steps(code: seq[Instruction]): tuple[pc: int16; acc: int] =
     of opNop:
       inc pc
     of opAcc:
-      inc acc, code[pc].offset
+      inc acc, code[pc].val
       inc pc
     of opJmp:
-      inc pc, code[pc].offset
+      inc pc, code[pc].val
 
+
+var code: seq[Instruction]
+
+# Load the program from file.
+for line in "p8.data".lines:
+  let fields = line.split(' ')
+  code.add (parseEnum[OpCode](fields[0]), parseInt(fields[1]))
+
+
+### Part 1 ###
 
 proc findLoop(code: seq[Instruction]): int =
   ## Detect first looping instruction and return the accumulator value at this moment.
-
   var history: set[int16]
   for (pc, acc) in code.steps():
     if pc in history: return acc
-    history.incl(pc)
+    history.incl pc
 
+echo "Part 1: ", code.findLoop()
+
+
+### Part 2 ###
 
 proc correct(code: var seq[Instruction]): int =
   ## Correct the code and return the value of accumulator after execution.
@@ -54,20 +62,9 @@ proc correct(code: var seq[Instruction]): int =
     for (pc, acc) in code.steps():
       if pc >= code.len: return acc   # Terminating.
       if pc in history: break         # Looping.
-      history.incl(pc)
+      history.incl pc
 
     # Restore the previous opcode.
     code[idx].op = prevOp
 
-
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
-var code: seq[Instruction]
-
-# Load the program from file.
-for line in "data".lines:
-  let fields = line.split(' ')
-  code.add((parseEnum[OpCode](fields[0]), parseInt(fields[1])))
-
-echo "Part 1: ", code.findLoop()
 echo "Part 2: ", code.correct()
