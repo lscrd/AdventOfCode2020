6b84aec9866cd84591c0451c606c9e7811ec7d7a
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:22:55 +0200

    Update p17.nim

--- Day 17/p17.nim
+++ Day 17/p17.nim
@@ -1,14 +1,9 @@
 # We use 4D coordinates for both parts. In part 1, the w-coordinate is always 0.
 
-import sets
-
-const
-  Max =  1_000_000
-  Min = -1_000_000
+import std/[algorithm, sets]
 
 type
-
-  Coords = tuple[x, y, z, w: int]
+  Coords = array[4, int]      # Use an array rather than a tuple.
   CoordSet = HashSet[Coords]
 
   # Description of active cells.
@@ -18,7 +13,7 @@ type
     coords: CoordSet      # Coordinates of active cells.
 
   # Ranges to use when updating state.
-  Ranges = tuple[x, y, z, w: Slice[int]]
+  Ranges = array[4, Slice[int]]
 
 
 func initActiveCells(coords: CoordSet; useW: bool): ActiveCells =
@@ -33,43 +28,42 @@ func activeCount(activeCells: ActiveCells; coords: Coords): int =
       for dz in -1..1:
         for dw in activeCells.deltaW:
           if (dx, dy, dz, dw) != (0, 0, 0, 0):
-            if (coords.x + dx, coords.y + dy, coords.z + dz, coords.w + dw) in activeCells.coords:
+            if [coords[0] + dx, coords[1] + dy,
+                coords[2] + dz, coords[3] + dw] in activeCells.coords:
               inc result
 
 
 func ranges(activeCells: ActiveCells): Ranges =
   ## Return the ranges to use to search for cells to activate or deactivate in next cycle.
 
-  var
-    minx, miny, minz, minw = Max
-    maxx, maxy, maxz, maxw = Min
+  const
+    Max =  1_000_000
+    Min = -1_000_000
 
-  for (x, y, z, w) in activeCells.coords:
-    if x < minx: minx = x
-    elif x > maxx: maxx = x
-    if y < miny: miny = y
-    elif y > maxy: maxy = y
-    if z < minz: minz = z
-    elif z > maxz: maxz = z
-    if w < minw: minw = w
-    elif w > maxw: maxw = w
-    result = ((minx-1)..(maxx+1), (miny-1)..(maxy+1), (minz-1)..(maxz+1), (minw-1)..(maxw+1))
+  var minCoords, maxCoords: array[4, int]
+  minCoords.fill(Max)
+  maxCoords.fill(Min)
 
-  # Deactivate the w-coordinate if working in 3D.
-  if not activeCells.useW: result.w = 0..0
+  for coords in activeCells.coords:
+    for i, coord in coords:
+      if coord < minCoords[i]: minCoords[i] = coord
+      elif coord > maxCoords[i]: maxCoords[i] = coord
+
+  for i in 0..3:
+    result[i] = (minCoords[i] - 1)..(maxCoords[i] + 1)
 
 
 func doCycle(activeCells: var ActiveCells) =
   ## Execute a cycle, updating set of active cells.
 
-  let (xrange, yrange, zrange, wrange) = activeCells.ranges
+  let r = activeCells.ranges
   var current = activeCells   # Make a copy before updating.
 
-  for x in xrange:
-    for y in yrange:
-      for z in zrange:
-        for w in wrange:
-          let coords = (x, y, z, w)
+  for x in r[0]:
+    for y in r[1]:
+      for z in r[2]:
+        for w in r[3]:
+          let coords = [x, y, z, w]
           let actCount = current.activeCount(coords)
           # Update set of active cells.
           if coords in current.coords:
@@ -79,22 +73,28 @@ func doCycle(activeCells: var ActiveCells) =
             activeCells.coords.incl(coords)
 
 
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
 var initCoords: CoordSet
 
 # Get the initial coordinates.
-var y = 0
-for line in "data".lines:
-  for x, c in line:
-    if c == '#': initCoords.incl((x, y, 0, 0))
-  inc y
+var coords: Coords
+for line in "p17.data".lines:
+  for c in line:
+    if c == '#': initCoords.incl coords
+    inc coords[0]
+  inc coords[1]
+  coords[0] = 0
 
 var activeCells: ActiveCells
+
+
+### Part 1 ###
 
 activeCells = initActiveCells(initCoords, false)
 for _ in 1..6: activeCells.doCycle()
 echo "Part1: ", activeCells.coords.len
+
+
+### Part 2 ###
 
 activeCells = initActiveCells(initCoords, true)
 for _ in 1..6: activeCells.doCycle()
