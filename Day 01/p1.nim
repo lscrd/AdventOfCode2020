f6a7ff093970744f9477d80a6bacc0e30a4b9786
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 22:54:37 +0200

    Update p1.nim

--- Day 01/p1.nim
+++ Day 01/p1.nim
@@ -1,8 +1,11 @@
-import strutils
+import std/strutils
 
 var data: seq[int]
-for line in "data".lines():
-  data.add(parseInt(line))
+for line in "p1.data".lines():
+  data.add line.parseInt()
+
+
+### Part 1 ###
 
 func part1(data: seq[int]): int =
   for i in 0..<data.high:
@@ -10,6 +13,11 @@ func part1(data: seq[int]): int =
     for j in i..data.high:
       if val + data[j] == 2020:
         return val * data[j]
+
+echo "Part 1: ", data.part1()
+
+
+### Part 2 ###
 
 func part2(data: seq[int]): int =
   for i in 0..(data.high-2):
@@ -20,5 +28,4 @@ func part2(data: seq[int]): int =
         if val1 + val2 + data[k] == 2020:
           return val1 * val2 * data[k]
 
-echo "Part 1: ", data.part1()
 echo "Part 2: ", data.part2()
