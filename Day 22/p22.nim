fd2b2758120d057cfbe0a71075f98179099ee8a3
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:29:36 +0200

    Update p22.nim

--- Day 22/p22.nim
+++ Day 22/p22.nim
@@ -1,13 +1,12 @@
-## We represent a deck as a sequence. Despite the O(n) deletion of first element,
-## for some reason, using a deque is less efficient.
-
-import sets, strutils
+import std/[sets, strutils]
 
 type
   Deck = seq[int8]
   Decks = array[1..2, Deck]
 
-#---------------------------------------------------------------------------------------------------
+var
+  decks: Decks
+  winner: int
 
 proc loadDecks(filename: string): Decks =
   ## Load decks from a file.
@@ -16,16 +15,15 @@ proc loadDecks(filename: string): Decks =
     if line.startsWith("Player"):
       inc idx
     elif line.len > 0:
-      result[idx].add(line.parseInt.int8)
-
-#---------------------------------------------------------------------------------------------------
+      result[idx].add line.parseInt.int8
 
 proc score(deck: Deck): int =
   ## Return the score of a deck.
   for i in 1..deck.len:
     inc result, deck[^i] * i
 
-#---------------------------------------------------------------------------------------------------
+
+### Part 1 ###
 
 proc play1(decks: var Decks): int =
   ## Play, using first rules, and return the winner of game.
@@ -40,22 +38,28 @@ proc play1(decks: var Decks): int =
 
     # Find winner of round and update decks.
     if card1 > card2:
-      decks[1].add(card1)
-      decks[1].add(card2)
+      decks[1].add card1
+      decks[1].add card2
     else:
-      decks[2].add(card2)
-      decks[2].add(card1)
+      decks[2].add card2
+      decks[2].add card1
 
     # Check if game has a winner.
     if decks[2].len == 0:
       return 1
-    elif decks[1].len == 0:
+    if decks[1].len == 0:
       return 2
 
-#---------------------------------------------------------------------------------------------------
+
+decks = loadDecks("p22.data")
+winner = decks.play1()
+echo "Part 1: ", decks[winner].score
+
+
+### Part 2 ###
 
 proc play2(decks: var Decks): int =
-  ## Play, using second rules, and return the winner.
+  ## Play, using second rules, and return the winner of game.
 
   var history: HashSet[seq[int8]]
   var winner: int
@@ -66,7 +70,7 @@ proc play2(decks: var Decks): int =
     let state = decks[1] & 0 & decks[2]
     if state in history:
       return 1
-    history.incl(state)
+    history.incl state
 
     # Deal cards.
     let card1 = decks[1][0]
@@ -85,25 +89,17 @@ proc play2(decks: var Decks): int =
 
     # Update decks.
     if winner == 1:
-      decks[1].add(card1)
-      decks[1].add(card2)
+      decks[1].add card1
+      decks[1].add card2
     else:
-      decks[2].add(card2)
-      decks[2].add(card1)
+      decks[2].add card2
+      decks[2].add card1
 
     # Check if game or subgame has a winner.
     if decks[1].len == 0 or decks[2].len == 0:
       return winner
 
-#———————————————————————————————————————————————————————————————————————————————————————————————————
 
-var decks: Decks
-var winner: int
-
-decks = loadDecks("data")
-winner = decks.play1()
-echo "Part 1: ", decks[winner].score
-
-decks = loadDecks("data")
+decks = loadDecks("p22.data")
 winner = decks.play2()
 echo "Part 2: ", decks[winner].score
