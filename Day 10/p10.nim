261313649e6f19e8584c1e71c6906a02a4141d02
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:10:50 +0200

    Update p10.nim

--- Day 10/p10.nim
+++ Day 10/p10.nim
@@ -1,9 +1,16 @@
-import algorithm, strutils
+import std/[algorithm, strutils]
 
 type Differences = array[1..3, Natural]
 
+# Load data.
 var outputs: seq[Natural]
+for line in "p10.data".lines:
+  outputs.add line.parseInt()
 
+outputs.sort()
+outputs.add outputs[^1] + 3
+
+### Part 1 ###
 
 func differences(outputs: seq[Natural]): Differences =
   ## Compute the differences between consecutive values.
@@ -12,30 +19,20 @@ func differences(outputs: seq[Natural]): Differences =
     inc result[jolts - current]
     current = jolts
 
+let diff = outputs.differences
+echo "Part 1: ", diff[1] * diff[3]
+
+
+### Part 2 ###
 
 func wayCount(outputs: seq[Natural]; target: Natural): Natural =
   ## Count the number of ways to get the target value with the given outputs.
 
-  var counts = newSeq[Natural](target + 1)    # Maps jolt value to number of ways.
+  var counts = newSeq[Natural](target + 1)    # Number of ways to get a jolt value.
   counts[0] = 1
   for newVal in outputs:
     for val in max(newVal - 3, 0)..(newVal - 1):
       inc counts[newVal], counts[val]
   result = counts[target]
 
-
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
-# Load data.
-for line in "data".lines:
-  outputs.add(line.parseInt())
-
-outputs.sort()
-outputs.add(outputs[^1] + 3)
-
-# Compute differences and output the requested result.
-let diff = outputs.differences
-echo "Part 1: ", diff[1] * diff[3]
-
-# Compute the number of ways to connect the adapters.
 echo "Part 2: ", outputs.wayCount(outputs[^1])
