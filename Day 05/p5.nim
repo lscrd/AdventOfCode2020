8d8abc84abf8617cc974ea40d3c86b012a8c9f21
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:04:15 +0200

    Update p5.nim

--- Day 05/p5.nim
+++ Day 05/p5.nim
@@ -1,5 +1,5 @@
 type
-  SeatId = range[0..1023]
+  SeatId = 0..1023
   SeatSet = set[SeatId]
 
 func decode(str: string; lowChar: char): Natural =
@@ -20,27 +20,32 @@ func seatId(seatCode: string): SeatId =
   let col = seatCode[7..9].decode('L')
   result = row * 8 + col
 
+var seats: SeatSet
+for line in "p5.data".lines:
+  seats.incl line.seatId
+
+
+### Part 1 ###
+
 func maxId(seats: SeatSet): SeatId =
   ## Find the higer seat ID in a set of seat IDs.
   result = SeatId.high
   while result notin seats:
     dec result
 
+echo "Part 1: ", seats.maxId
+
+
+### Part 2 ###
+
 func findSeatFrom(seats: SeatSet): SeatId =
   ## Find a seat ID from a set of seat IDs.
   var candidates = {8..1015}   # Ignore first and last row.
   for id in 8..1015:
-    if id in seats or id - 1 notin seats or id + 1 notin seats:
-      candidates.excl(id)
+    if id in seats or (id - 1) notin seats or (id + 1) notin seats:
+      candidates.excl id
   assert candidates.card == 1
   for id in candidates:
     return id
 
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
-var seats: SeatSet
-for line in "data".lines:
-  seats.incl(seatId(line))
-
-echo "Part 1: ", maxId(seats)
 echo "Part 2: ", findSeatFrom(seats)
