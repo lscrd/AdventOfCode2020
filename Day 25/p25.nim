bb07c12b51b3fe20c4f47f066c624398a4b9aba3
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:32:37 +0200

    Update p25.nim

--- Day 25/p25.nim
+++ Day 25/p25.nim
@@ -1,30 +1,36 @@
+import std/strutils
 
 func loop(subjectNumber, size: Natural): Natural =
   ## Run the loop "size" times and return the result.
   result = 1
   for i in 1..size:
-    result *= subjectNumber
-    result = result mod 20201227
+    result = result * subjectNumber mod 20201227
 
 func loopSize(subjectNumber, target: Natural): Natural =
   ## Find the number of times to run the loop to get the target result.
   var value = 1
   while value != target:
     inc result
-    value *= subjectNumber
-    value = value mod 20201227
+    value = value * subjectNumber mod 20201227
 
-const
-  CardPublicKey = 15113849
-  DoorPublicKey = 4206373
 
-let cardLoopSize = loopSize(7, CardPublicKey)
-let doorLoopSize = loopSize(7, DoorPublicKey)
+### Part 1 ###
 
-let encryptionKey = loop(4206373, cardLoopSize)
-if loop(15113849, doorLoopSize) == encryptionKey:
+let infile = open("p25.data")
+let cardPublicKey = infile.readLine().parseInt()
+let doorPublicKey = infile.readline().parseInt()
+infile.close()
+
+let cardLoopSize = loopSize(7, cardPublicKey)
+let doorLoopSize = loopSize(7, doorPublicKey)
+
+let encryptionKey = loop(doorPublicKey, cardLoopSize)
+if loop(cardPublicKey, doorLoopSize) == encryptionKey:
   echo "Part 1: ", encryptionKey
 else:
-  echo "Inconsistency detected"
+  echo "Inconsistency detected."
+
+
+### Part 2 ###
 
 echo "Part 2: done"
