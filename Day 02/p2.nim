596ddb6d0435371f96f640d434d34d7e49531d95
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 22:56:51 +0200

    Update p2.nim

--- Day 02/p2.nim
+++ Day 02/p2.nim
@@ -1,16 +1,6 @@
-import strscans, strutils
+import std/[strscans, strutils]
 
 type ValidationProc = proc(password: string; letter: char; val1, val2: Natural): bool
-
-
-proc validateCount(password: string; letter: char; min, max: Natural): bool =
-  ## Check if there is between "min" and "max" occurrences of "letter" in "password".
-  result = password.count(letter) in min..max
-
-
-proc validatePositions(password: string; letter: char; pos1, pos2: Natural): bool =
-  ## Check if "letter" is present in "password" at exactly one of positions "pos1" or "pos2".
-  result = password[pos1 - 1] == letter xor password[pos2 - 1] == letter
 
 
 proc validPasswordsCount(filename: string; isValid: ValidationProc): Natural =
@@ -21,15 +11,29 @@ proc validPasswordsCount(filename: string; isValid: ValidationProc): Natural =
     letterString, password: string
 
   var linenum = 0
-  for line in "data".lines:
+  for line in filename.lines:
     inc linenum
     if not line.scanf("$i-$i $w: $+$.", val1, val2, letterString, password):
       raise newException(ValueError, "wrong content at line " & $linenum)
     if letterString.len != 1 or letterString[0] notin Letters:
       raise newException(ValueError, "wrong letter at line " & $linenum)
-    if isValid(password, letterString[0], val1, val2):
+    if password.isValid(letterString[0], val1, val2):
       inc result
 
 
-echo "Part 1: ", "data".validPasswordsCount(validateCount)
-echo "Part 2: ", "data".validPasswordsCount(validatePositions)
+### Part 1 ###
+
+proc validateCount(password: string; letter: char; min, max: Natural): bool =
+  ## Check if there is between "min" and "max" occurrences of "letter" in "password".
+  result = password.count(letter) in min..max
+
+echo "Part 1: ", "p2.data".validPasswordsCount(validateCount)
+
+
+### Part 2 ###
+
+proc validatePositions(password: string; letter: char; pos1, pos2: Natural): bool =
+  ## Check if "letter" is present in "password" at exactly one of positions "pos1" or "pos2".
+  result = password[pos1 - 1] == letter xor password[pos2 - 1] == letter
+
+echo "Part 2: ", "p2.data".validPasswordsCount(validatePositions)
