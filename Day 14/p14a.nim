53d0e946e2030b1193cf831ade78714dc22d2017
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:15:23 +0200

    Update p14a.nim

--- Day 14/p14a.nim
+++ Day 14/p14a.nim
@@ -2,7 +2,7 @@
    When dealing directly with bits, we have to take in account the endianness.
 ]#
 
-import bitops, strmisc, strscans, strutils, tables
+import std/[bitops, strmisc, strscans, strutils, tables]
 
 const Bits = 36
 
@@ -22,21 +22,33 @@ type
   # Memory is represented by a table as addresses are not contiguous and maybe very high.
   Memory = Table[Natural, Value]
 
-#---------------------------------------------------------------------------------------------------
+var mem: Memory
+
 
 proc sum(mem: Memory): Value =
   ## Return the sum of values in memory.
   for value in mem.values:
     result += value
 
-#---------------------------------------------------------------------------------------------------
-
 template getMaskElem(mask: string; i: int): char =
   ## Return the mask character at index "i" taking in account the endianness.
   when cpuEndian == bigEndian: mask[i]
   else: mask[^(i+1)]
 
-#---------------------------------------------------------------------------------------------------
+
+# Load program.
+var program: seq[Instruction]
+for line in "p14.data".lines:
+  let (head, _, tail) = line.partition(" = ")
+  if head == "mask":
+    program.add Instruction(op: opSetMask, mask: tail)
+  else:
+    var address: int
+    discard head.scanf("mem[$i]", address)
+    program.add Instruction(op: opWriteMem, address: address, value: Value(tail.parseInt()))
+
+
+### Part 1 ###
 
 proc run1(mem: var Memory; program: seq[Instruction]) =
   ## Run the program using version 1 of decoder chip.
@@ -45,10 +57,8 @@ proc run1(mem: var Memory; program: seq[Instruction]) =
 
   for inst in program:
     case inst.op
-
-    of opSetMask:
+    of opSetMask:# Load program.
       mask = inst.mask
-
     of opWriteMem:
       var val = inst.value
       for i in 0..<Bits:
@@ -58,7 +68,11 @@ proc run1(mem: var Memory; program: seq[Instruction]) =
         else: val.clearBit(i)
         mem[inst.address] = val
 
-#---------------------------------------------------------------------------------------------------
+mem.run1(program)
+echo "Part 1: ", sum(mem)
+
+
+### Part 2 ###
 
 proc expand(mask: string): seq[string] =
   ## Expand a mask containing floating bits to a list of binary strings.
@@ -69,22 +83,20 @@ proc expand(mask: string): seq[string] =
     var next: seq[string]
 
     # Check bit "idx" of each mask.
-    for mask in result.items:
+    for mask in result:
       if mask[idx] == 'X':
-        # Expand the floating bit in two masks.
-        next.add(mask)
+        # Expand the floating bit as two masks.
+        next.add mask
         next[^1][idx] = '0'
-        next.add(mask)
+        next.add mask
         next[^1][idx] = '1'
       else:
         # Keep the mask as is (for now).
-        next.add(mask)
+        next.add mask
 
     # Prepare to process next bit.
     inc idx
-    result.shallowCopy(next)
-
-#---------------------------------------------------------------------------------------------------
+    result = move(next)
 
 proc run2(mem: var Memory; program: seq[Instruction]) =
   ## Run the program using version 2 of decoder chip.
@@ -93,37 +105,18 @@ proc run2(mem: var Memory; program: seq[Instruction]) =
 
   for inst in program:
     case inst.op
-
     of opSetMask:
       # No need to take in account the endianness as we will not do bit operations.
       mask = inst.mask
-
     of opWriteMem:
       # Build the address mask by combining address and mask.
       var addressMask = inst.address.toBin(Bits)
       for i, bit in mask:
         if bit != '0': addressMask[i] = bit
-      # Expand address mask to addresses and set the value in memory.
+      # Expand address mask to addresses and set the values in memory.
       for addressString in addressMask.expand():
         let address = addressString.parseBinInt()
         mem[address] = inst.value
-
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
-# Load program.
-var program: seq[Instruction]
-for line in "data".lines:
-  let (head, _, tail) = line.partition(" = ")
-  if head == "mask":
-    program.add(Instruction(op: opSetMask, mask: tail))
-  else:
-    var address: int
-    discard head.scanf("mem[$i]", address)
-    program.add(Instruction(op: opWriteMem, address: address, value: Value(tail.parseInt)))
-
-var mem: Memory
-mem.run1(program)
-echo "Part 1: ", sum(mem)
 
 mem.clear()
 mem.run2(program)
