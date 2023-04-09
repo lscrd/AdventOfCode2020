4a4a72b3ad9f89b002de28d4a48aa5c5d85a8d35
Luc Secouard <luc.secouard@laposte.net>
2023-04-09 23:21:41 +0200

    Update p16.nim

--- Day 16/p16.nim
+++ Day 16/p16.nim
@@ -1,106 +1,13 @@
-import sequtils, strscans, strutils, tables
+import std/[sequtils, strscans, strutils, tables]
 
 type
-
   Ranges = array[2, Slice[int]]
   Rules = Table[string, Ranges]
-
   Ticket = seq[int]
   Tickets = seq[Ticket]
 
-  Position = range[0..31]
-  PosSet = set[Position]
-  Positions = Table[string, PosSet]
-
-
-#---------------------------------------------------------------------------------------------------
-
-func invalidValue(ticket: Ticket; rules: Rules): int =
-  ## Check ticket against rules and return the invalid value or 0 if the ticket is valid.
-
-  for value in ticket:
-    block checkTicket:
-      for ranges in rules.values():
-        if value in ranges[0] or value in ranges[1]:
-          break checkTicket
-      # No compatible rule.
-      return value
-
-#---------------------------------------------------------------------------------------------------
-
-func checkValidity(tickets: Tickets; rules: Rules): tuple[tser: int; tickets: Tickets] =
-  ## Check validity of tickets.
-  ## Return the "ticket scanning error rate" (TSER) and the list of valid tickets.
-
-  for ticket in tickets:
-    let val = ticket.invalidValue(rules)
-    result.tser += val
-    if val == 0:
-      result.tickets.add(ticket)
-
-#---------------------------------------------------------------------------------------------------
-
-func update(positions: var Positions; ticket: Ticket; rules: Rules) =
-  ## Check ticket against rules and remove impossible positions for rule fields.
-
-  for pos, val in ticket:
-    for rule, ranges in rules.pairs:
-      if val notin ranges[0] and val notin ranges[1]:
-        positions[rule].excl(pos)
-
-#---------------------------------------------------------------------------------------------------
-
-func value(posSet: PosSet): Position =
-  ## Return the first position in a PosSet and in fact the only
-  ## one as this function is used for sets containing a single position.
-  for pos in posSet: return pos
-
-#---------------------------------------------------------------------------------------------------
-
-func remove(positions: var Positions; pos: Position) =
-  ## Remove a position from all positions containing more than one element.
-  for name, position in positions.pairs:
-    if position.card > 1:
-      positions[name].excl(pos)
-
-#---------------------------------------------------------------------------------------------------
-
-func reduce(positions: var Positions) =
-  ## Reduce the number of possible positions until they are all singletons.
-
-  var changed = true
-  var done: PosSet
-  while changed:
-    changed = false
-    for name, posset in positions.pairs:
-      if posset.card == 1 and not (posset <= done):
-        let pos = posset.value
-        positions.remove(pos)
-        done.incl(pos)
-        changed = true
-
-#---------------------------------------------------------------------------------------------------
-
-func fieldPositions(tickets: Tickets; rules: Rules): Positions =
-  ## Return the position of fields by examining tickets.
-
-  # Initialize possible positions.
-  let maxpos = tickets[0].high
-  for name in rules.keys:
-    result[name] = {Position(0)..Position(maxpos)}
-
-  # Remove positions by examining each nearby ticket field value.
-  for ticket in tickets:
-    result.update(ticket, rules)
-
-  # Reduce the number of possible positions to singletons.
-  result.reduce()
-
-
-#———————————————————————————————————————————————————————————————————————————————————————————————————
-
 # Read and split input file in three strings: rules, my ticket and nearby tickets.
-let data = "data".readFile().split("\n\n")
+let data = "p16.data".readFile().split("\n\n")
 
 # Parse rules and build rules table.
 var rules: Rules
@@ -120,19 +27,102 @@ for line in data[1].splitLines():
 var tickets: Tickets
 for line in data[2].splitLines():
   if line.len == 0 or line[0] notin Digits: continue
-  tickets &= line.split(',').map(parseInt)
+  tickets.add line.split(',').map(parseInt)
+
+
+### Part 1 ###
+
+func invalidValue(ticket: Ticket; rules: Rules): int =
+  ## Check ticket against rules and return the invalid value or 0 if the ticket is valid.
+  for value in ticket:
+    block checkValue:
+      for ranges in rules.values():
+        if value in ranges[0] or value in ranges[1]:
+          break checkValue
+      # No compatible rule.
+      return value
+
+func checkValidity(tickets: Tickets; rules: Rules): tuple[tser: int; tickets: Tickets] =
+  ## Check validity of tickets.
+  ## Return the "ticket scanning error rate" (TSER) and the list of valid tickets.
+  for ticket in tickets:
+    let val = ticket.invalidValue(rules)
+    result.tser += val
+    if val == 0:
+      result.tickets.add ticket
 
 # Check validity of nearby tickets.
 let (tser, validTickets) = tickets.checkValidity(rules)
+
+echo "Part 1: ", tser
+
+
+### Part 2 ###
+
+type
+  Position = range[0..31]           # Actually range[0..19].
+  PosSet = set[Position]
+  Positions = Table[string, PosSet]
+
+
+func update(positions: var Positions; ticket: Ticket; rules: Rules) =
+  ## Check ticket against rules and remove impossible positions for rule fields.
+  for pos, val in ticket:
+    for fieldName, ranges in rules.pairs:
+      if val notin ranges[0] and val notin ranges[1]:
+        positions[fieldName].excl pos
+
+
+func value(posSet: PosSet): Position =
+  ## Return the first position in a PosSet and in fact the only one
+  ## as the set is a singleton.
+  for pos in posSet: return pos
+
+
+func remove(positions: var Positions; pos: Position) =
+  ## Remove a position from all positions containing more than one element.
+  for fieldName, position in positions.pairs:
+    if position.card > 1:
+      positions[fieldName].excl pos
+
+
+func reduce(positions: var Positions) =
+  ## Reduce the number of possible positions until they are all singletons.
+  var changed = true
+  var done: PosSet
+  while changed:
+    changed = false
+    for posSet in positions.values:
+      if posSet.card == 1 and not (posSet <= done):
+        let pos = posSet.value
+        positions.remove pos
+        done.incl pos
+        changed = true
+
+
+func fieldPositions(tickets: Tickets; rules: Rules): Positions =
+  ## Return the position of fields by examining tickets.
+
+  # Initialize possible positions.
+  let maxPos = tickets[0].high
+  for fieldName in rules.keys:
+    result[fieldName] = {Position(0)..Position(maxPos)}
+
+  # Remove positions by examining each nearby ticket field value.
+  for ticket in tickets:
+    result.update(ticket, rules)
+
+  # Reduce the number of possible positions to singletons.
+  result.reduce()
+
 
 # Find field positions.
 let positions = fieldPositions(validTickets, rules)
 
 # Compute the product of values of fields starting with "departure".
 var product = 1
-for name, posSet in positions.pairs:
-  if name.startswith("departure"):
+for fieldName, posSet in positions.pairs:
+  if fieldName.startswith("departure"):
     product *= myTicket[posSet.value]
 
-echo "Part 1: ", tser
 echo "Part 2: ", product
